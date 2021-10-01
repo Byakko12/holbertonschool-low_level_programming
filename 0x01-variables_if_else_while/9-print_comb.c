aa86fe4ab1014b1e6e3c583a822f1e2a4377be23
David <davidlandg@gmail.com>
2021-10-01 16:40:54 -0500

    print_comb and updates

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print numbers all single numbers of base 10 with commas
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(',');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
