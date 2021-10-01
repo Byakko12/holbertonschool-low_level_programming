6b896e935a91b3ba19dca3a6d0fdcea954e2de28
David <davidlandg@gmail.com>
2021-10-01 15:40:11 -0500

    Add - print_alphabet and updates

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print if the number is negative positive or 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
