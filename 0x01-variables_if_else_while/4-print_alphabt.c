be9d1eb0a91f9be78ab94bc19a45d993a1c46123
David <davidlandg@gmail.com>
2021-10-01 16:02:01 -0500

    Fix - code fixed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,18 +5,18 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 97; i <= 122; i++)
-    {
-        if (i == 101 || i == 113)
-        {
-        }
-        else
-        {
-            putchar(i);
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 101 || i == 113)
+		{
+		}
+		else
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
