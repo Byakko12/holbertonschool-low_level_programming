6361a29fd016fec2351696faf87999b2fe2dee50
David <davidlandg@gmail.com>
2021-10-05 14:19:07 -0500

    Add - print_to_98 and updates

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,40 +1,93 @@
 #include "main.h"
+/**
+ * print_to_98 - print to 98
+ * @n: value to start
+ * Return: void
+ */
 
 void print_to_98(int n)
 {
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			if (n >= 100)
+			{
+				_putchar((n / 100) + '0');
+				_putchar(((n / 10) % 10) + '0');
+				_putchar((n % 10) + '0');
+				_putchar(',');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			if (n < 98)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+	else if (n < 0)
+	{
+		for (; n != 0 && n <= 10; n--)
+		{
+			if (n == -10)
+			{
+				n = -n;
+				_putchar('-');
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			else
+			{
+				_putchar('-');
+				_putchar(n + '0');
+			}
+			if (n > 0)
+			{
+				_putchar(',');
+			}
 
-    if (n > 98)
-    {
-        for (; n > 98; n--)
-        {
-            _putchar((n / 10) + '0');
-            _putchar((n % 10) + '0');
-            _putchar(',');
-            if (n == '\0')
-            {
-                _putchar('\n');
-            }
-        }
-    }
-    else
-    {
-        for (; n <= 98; n++)
-        {
-            if (n >= 0 && n < 10)
-            {
-                _putchar((n %= 10) + '0');
-                _putchar(',');
-            }
-            else if (n > 9)
-            {
-                _putchar((n / 10) + '0');
-                _putchar((n % 10) + '0');
-                _putchar(',');
-            }
-            if (n == '\0')
-            {
-                _putchar('\n');
-            }
-        }
-    }
+		}
+		for (; n <= 98; n++)
+		{
+			if (n >= 0 && n < 10)
+			{
+				_putchar((n % 10) + '0');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			if (n < 98)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (; n <= 98; n++)
+		{
+			if (n >= 0 && n < 10)
+			{
+				_putchar((n % 10) + '0');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			if (n < 98)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
 }
