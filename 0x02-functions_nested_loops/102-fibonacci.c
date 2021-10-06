532f30eee99f836499fde553d09ba626aa8649ca
David <davidlandg@gmail.com>
2021-10-05 21:06:40 -0500

    Fix - code fixed

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,13 +7,19 @@ int main(void)
 {
 	long int i, size = 50, sum = 0, n1 = 1, n2 = 1;
 
-	printf("%li, ", n2);
+	printf("%li", n2);
 	for (i = 1; i < size; i++)
 	{
+		if (i < size)
+		{
+			printf(", ");
+		}
+		
 		sum = (n1 + n2);
-		printf("%li, ", sum);
+		printf("%li", sum);
 		n1 = n2;
 		n2 = sum;
 	}
+	printf("\n");
 	return (0);
 }
