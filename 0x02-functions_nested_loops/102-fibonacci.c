fe8b06607f04b50c5010ab2891f8b3a14eb5b310
David <davidlandg@gmail.com>
2021-10-05 20:52:27 -0500

    Add - fibonacci and updates

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Prints sequence of forst 50 fibonacci numbers starting with 1
+ * Return: not return
+ */
+int main(void)
+{
+	int i, size = 50, sum = 0, n1 = 1, n2 = 1;
+
+	printf("%d, %d, ", n1, n2);
+	for (i = 1; i <= size; i++)
+	{
+		sum = (n1 + n2);
+		printf("%d, ", sum);
+		n1 = n2;
+		n2 = sum;
+	}
+}
