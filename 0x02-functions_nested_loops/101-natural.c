7b8b22c1aadb46eb6f08823dc42ab02748273e47
David <davidlandg@gmail.com>
2021-10-05 20:12:26 -0500

    Add - natural and updates

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - sum all numbers below 10 multiples of 3 and 5, except 1024
+ * Return: 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 5) == 0 || (i % 3))
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
