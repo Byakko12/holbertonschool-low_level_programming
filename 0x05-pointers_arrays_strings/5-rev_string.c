e1d5d9208b44f89814663d4329c1054478ed55c5
David <davidlandg@gmail.com>
2021-10-11 18:56:12 -0500

    Fix - code fixed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,24 @@
 #include "main.h"
-
+/**
+ * rev_string - reverses a string
+ * @s: pointer to string
+ * Return: not return
+ */
 void rev_string(char *s)
 {
-    int i, j;
-    char b[20];
+	int i, j;
+	char b[1000];
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-    }
-    for (j = 0; i > 0; i--)
-    {
-        b[j] = s[i - 1];
-        j++;
-    }
-    for (; i < j; i++)
-    {
-        s[i] = b[i];
-    }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; i > 0; i--)
+	{
+		b[j] = s[i - 1];
+		j++;
+	}
+	for (; i < j; i++)
+	{
+		s[i] = b[i];
+	}
 }
