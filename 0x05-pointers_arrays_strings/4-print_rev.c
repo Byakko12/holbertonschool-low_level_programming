8b1ab9a736b12c4bd1e4e94245cd8d2df07dee90
David <davidlandg@gmail.com>
2021-10-12 00:49:31 -0500

    Fix - code fixed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+/**
+ * print_rev - print string in reverse
+ * @s: string pointer
+ * Return: not return
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; i[s] != '\0'; i++)
+	{
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
