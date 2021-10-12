e800e7d726ad7e5693bd35baa158e806160e18c7
David <davidlandg@gmail.com>
2021-10-11 20:31:59 -0500

    Add - puts2 and updates

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,14 @@
  */
 void puts2(char *str)
 {
+	int i = 0, j = 0;
 
-	for (; *str != '\0'; str += 2)
+	for (; str[i] != '\0'; i++)
 	{
-		_putchar(*str);
+	}
+	for (; j < i; j += 2)
+	{
+		_putchar(str[j]);
 	}
 	_putchar('\n');
 }
