8241a828879e097fefb17f4ccf9beb678d06fd26
David <davidlandg@gmail.com>
2021-10-13 16:56:27 -0500

    Add - strncat and updates

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates two strings and size
+ * @dest: Array buffer
+ * @src: Array to append
+ * @n: size of array
+ * Return: dest with two strings concat
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	for (; dest[i] != '\0'; i++)
+	{
+	}
+	for (; i < n && src[j] != '\0'; j++)
+	{
+		dest[i++] = src[j];
+	}
+	return (dest);
+}
