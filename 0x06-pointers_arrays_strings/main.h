491e46d18a9849704da06087dd467bd63f614479
David <davidlandg@gmail.com>
2021-10-14 13:29:43 -0500

    Fix - code fixed

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,11 @@
 #ifndef HEADER_MAIN
 #define HEADER_MAIN
+#include <stdio.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-char *string_toupper(char *s)
-#endif
+char *string_toupper(char *s);
+#endif /* HEADER_MAIN */
