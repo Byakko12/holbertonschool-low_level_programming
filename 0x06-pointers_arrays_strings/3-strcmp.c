71d81007b443b280db004e100547b0da1a1056f4
David <davidlandg@gmail.com>
2021-10-14 14:05:50 -0500

    Fix - code fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
  * *_strcmp - compares two strings.
  * @s1: String number 1
  * @s2: String number 2
- * Return: return positive if s1 is greater than s2 
+ * Return: return positive if s1 is greater than s2
  * negatives if is less or 0 if is equal
  */
 int _strcmp(char *s1, char *s2)
@@ -18,7 +18,8 @@ int _strcmp(char *s1, char *s2)
 		}
 		else
 		{
-			return (s1[i] == s2[i]);
+			return (s1[i] - s2[i]);
 		}
 	}
+	return (s1[i] - s2[i]);
 }
