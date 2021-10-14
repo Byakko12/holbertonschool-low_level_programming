56bbbc8da4e13dcbfa3b08024f9d733d6324fc19
David <davidlandg@gmail.com>
2021-10-14 15:40:41 -0500

    Fix - code fixed

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,36 +6,36 @@
  */
 char *cap_string(char *str)
 {
-    int i;
+	int i;
 
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    if (str[0] == '\0')
-    {
-        return (str);
-    }
-    for (i = 0; str[i] != '\0'; i++)
-    {
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (str[0] == '\0')
+	{
+		return (str);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
 
-        if (str[i] == ' ' || str[i] == ',' ||
-            str[i] == '\t' || str[i] == '\n' ||
-            str[i] == ';' || str[i] == '.' ||
-            str[i] == '!' || str[i] == '?' ||
-            str[i] == '\"' || str[i] == '(' ||
-            str[i] == ')' || str[i] == '{' ||
-            str[i] == '}')
-        {
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-            {
-                str[i + 1] -= 32;
-            }
-            else if (str[0] == 0)
-            {
-                str[i] -= 32;
-            }
-        }
-    }
-    return (str);
+		if (str[i] == ' ' || str[i] == ',' ||
+			str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ';' || str[i] == '.' ||
+			str[i] == '!' || str[i] == '?' ||
+			str[i] == '\"' || str[i] == '(' ||
+			str[i] == ')' || str[i] == '{' ||
+			str[i] == '}')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] -= 32;
+			}
+			else if (str[0] == 0)
+			{
+				str[i] -= 32;
+			}
+		}
+	}
+	return (str);
 }
