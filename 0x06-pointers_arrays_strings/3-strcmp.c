f6755628926dff21f6534b6af9c8ef2e3bc084e9
David <davidlandg@gmail.com>
2021-10-14 14:22:21 -0500

    Fix - code fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,18 +8,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i = 0, temp;
 
 	for (; s1[i] != '\0'; i++)
 	{
 		if (s1[i] == s2[i])
 		{
-			return (0);
+			temp = 0;
 		}
 		else
 		{
-			return (s1[i] - s2[i]);
+			temp = (s1[i] - s2[i]);
+			break;
 		}
 	}
-	return (s1[i] - s2[i]);
+	return (temp);
 }
