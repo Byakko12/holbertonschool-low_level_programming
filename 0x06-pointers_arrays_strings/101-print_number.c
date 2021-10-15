903077a22c612a3916ef57b73a6535c1ebc19aff
David <davidlandg@gmail.com>
2021-10-14 20:29:42 -0500

    Add - print_numbers and update README

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,7 +1,36 @@
 #include "main.h"
-
+/**
+ * print_number - print numbers with _putchar
+ * @n: numbers
+ * Return: not return
+ */
 void print_number(int n)
 {
 
+	int last_digit = 0, reversed = 0;
 
+	if (n < 0)
+	{
+		putchar('-');
+		last_digit = ('0' - (n % 10));
+		n /= -10;
+	}
+	else
+	{
+		last_digit = ((n % 10) + '0');
+		n /= 10;
+	}
+	while (n > 0)
+	{
+		reversed = ((reversed * 10) + (n % 10));
+		n /= 10;
+	}
+	while (reversed > 0)
+	{
+		char digit = ((reversed % 10) + '0');
+
+		putchar(digit);
+		reversed /= 10;
+	}
+	putchar(last_digit);
 }
