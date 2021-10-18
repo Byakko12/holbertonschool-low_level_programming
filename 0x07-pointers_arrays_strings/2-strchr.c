a4f935c297eed20af3929f86be1197158bd04dce
David <davidlandg@gmail.com>
2021-10-18 13:24:50 -0500

    Fix - code fixed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * *_strchr - locates a character in a string
+ * @s: string to be evaluated
+ * @c: character to be comparated
+ * Return: string left
+ */
 char *_strchr(char *s, char c)
 {
 
