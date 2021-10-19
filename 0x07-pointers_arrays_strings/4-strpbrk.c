86339778d51b6016f75a104ea6b9c39e335dc73d
David <davidlandg@gmail.com>
2021-10-18 21:44:00 -0500

    Add - strpbrk function that return first match with a string and updates

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * *_strpbrk - searches a string for any of a set of bytes
+ * @s: string to be evaluated
+ * @accept: character to be comparated
+ * Return:  pointer to the byte in s or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *buf = accept;
+
+	while (*s != '\0')
+	{
+		for (accept = buf; *accept != '\0'; accept++)
+		{
+			if (*s == *accept)
+			{
+				return (s);
+				break;
+			}
+		}
+		s++;
+		if (*s != '\0')
+		{
+			return (NULL);
+		}
+	}
+	return (s);
+}
