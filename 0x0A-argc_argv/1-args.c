fde023581f088c13e36dd5f48624298daee93646
David <davidlandg@gmail.com>
2021-10-22 14:55:34 -0500

    ADD - args and  update README

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints its name, followed by a new line
+ * @argc: argument counter
+ * @argv: argument vector to store name and more datas
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    (void) argv;
+    for (i = 0; i < argc - 1; i++)
+    {
+    }
+    printf("%d\n", i);
+    return (0);
+}
