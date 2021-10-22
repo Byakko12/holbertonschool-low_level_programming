a33ffc9eddeb47715aa5a53beb01e3a4ac09c925
David <davidlandg@gmail.com>
2021-10-22 15:44:13 -0500

    ADD - mul and README updates

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main -  multiplies two numbers
+ * @argc: argument counter
+ * @argv: argument vector to store name and more datas
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1, multiplies = 1;
+
+	if (i > 2)
+	{
+		printf("error\n");
+		return (1);
+	}
+	for (; i < argc; i++)
+	{
+		multiplies *= atoi(argv[i]);
+	}
+	printf("%d\n", multiplies);
+	return (0);
+}
