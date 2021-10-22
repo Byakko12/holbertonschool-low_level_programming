8c93626a7d193d4f0751364da0170b0904644fbe
David <davidlandg@gmail.com>
2021-10-22 14:57:07 -0500

    FIX - args ident and comment

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * main - prints its name, followed by a new line
+ * main - prints the number of arguments passed into it
  * @argc: argument counter
  * @argv: argument vector to store name and more datas
  * Return: return 0
  */
 int main(int argc, char *argv[])
 {
-    int i = 0;
-    (void) argv;
-    for (i = 0; i < argc - 1; i++)
-    {
-    }
-    printf("%d\n", i);
-    return (0);
+	int i = 0;
+	(void)argv;
+	for (i = 0; i < argc - 1; i++)
+	{
+	}
+	printf("%d\n", i);
+	return (0);
 }
