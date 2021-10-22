41bfb9e626870f3ee56fdd5e454f3ee322a5e59e
David <davidlandg@gmail.com>
2021-10-22 16:33:54 -0500

    ADD - function add and updates

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * main - adds positive numbers
+ * @argc: argument counter
+ * @argv: argument vector to store name and more datas
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1, add = 0;
+
+	if (argc == 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for (; i < argc; i++)
+	{
+		if (*argv[i] <= '9' && *argv[i] >= '0')
+		{
+			add += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", add);
+	return (0);
+}
