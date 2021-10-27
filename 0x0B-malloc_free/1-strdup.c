794a1c6a8a8aeacd3decfcb05fca8b897bd83406
David <davidlandg@gmail.com>
2021-10-27 14:58:51 -0500

    ADD - strdup return a pointer to a newly space in memory and README updates

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * *_strdup - returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter
+ * @str: size of array
+ * Return: return a pointer to a newly allocated space in memory, or Null
+ */
+char *_strdup(char *str)
+{
+	char *buffer;
+	int i, lenght = _strlen(str);
+
+	buffer = malloc(sizeof(char) * lenght + 1);
+	if (buffer == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < lenght; i++)
+	{
+		buffer[i] = str[i];
+	}
+	return (buffer);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: array of strings
+ * Return: return length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
