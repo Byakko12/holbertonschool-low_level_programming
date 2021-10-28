d8c2c4db3f3cbced2beca041fa18c24aa2597da9
David <davidlandg@gmail.com>
2021-10-28 13:20:25 -0500

    ADD - free_grid free memory and README updates

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,18 @@
 #include "main.h"
-
+/**
+ * free_grid - frees a 2 dimensional grid previously created
+ * by your alloc_grid function
+ * @grid: array of integers
+ * @height: height of array
+ * Return: not return
+ */
 void free_grid(int **grid, int height)
-
 {
-    
+	int j;
+
+	for (j = 0; j < height; j++)
+	{
+		free(grid[j]);
+	}
+	free(grid);
 }
