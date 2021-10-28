ef2655d9af8178e8fb76257b52e424f4705a360e
David <davidlandg@gmail.com>
2021-10-28 13:14:37 -0500

    Fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		buffer[i] = (int *)malloc(width * sizeof(int *));
+		buffer[i] = (int *)malloc(width * sizeof(int));
 		if (buffer[i] == NULL)
 		{
 			for (j = 0; j < height; j++)
