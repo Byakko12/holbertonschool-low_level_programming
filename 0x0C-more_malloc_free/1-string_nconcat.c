ddddd9badea72dfe4b34a2acf8e93c9331435633
David <davidlandg@gmail.com>
2021-10-28 21:52:12 -0500

    FIX - string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,34 +1,40 @@
 #include "main.h"
-
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2:	seconde string
+ * @n: size of second string
+ * Return: pointer to a string concatenated, or NULL if fails
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int i, j, firstLenght, secondLenght;
-    char *buffer;
+	unsigned int i, j, firstLenght, secondLenght;
+	char *buffer;
 
-    firstLenght = _strlen(s1);
-    secondLenght = _strlen(s2);
-    if (n >= secondLenght)
-    {
-        n = firstLenght + secondLenght;
-    }
-    else
-    {
-        n = firstLenght + n;
-    }
-    buffer = malloc(sizeof(char) * n + 1);
-    if (buffer == NULL)
-    {
-        return (NULL);
-    }
-    for (i = 0; i < firstLenght; i++)
-    {
-        buffer[i] = s1[i];
-    }
-    for (j = 0; i < n; j++, i++)
-    {
-        buffer[i] = s2[j];
-    }
-    return (buffer);
+	firstLenght = _strlen(s1);
+	secondLenght = _strlen(s2);
+	if (n >= secondLenght)
+	{
+		n = firstLenght + secondLenght;
+	}
+	else
+	{
+		n = firstLenght + n;
+	}
+	buffer = malloc(sizeof(char) * n + 1);
+	if (buffer == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < firstLenght; i++)
+	{
+		buffer[i] = s1[i];
+	}
+	for (j = 0; i < n; j++, i++)
+	{
+		buffer[i] = s2[j];
+	}
+	return (buffer);
 }
 
 /**
@@ -38,10 +44,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
  */
 int _strlen(char *s)
 {
-    int i;
+	int i;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-    }
-    return (i);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
 }
