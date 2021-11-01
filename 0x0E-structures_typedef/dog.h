32c94ce77a06e8cf693f7708e3e4ce06a020b72c
David <davidlandg@gmail.com>
2021-11-01 18:36:23 -0500

    ADD - dog.h and README

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog - data of a dog
+ * @name: char store names
+ * @age: int store ages
+ * @owner: char store owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
