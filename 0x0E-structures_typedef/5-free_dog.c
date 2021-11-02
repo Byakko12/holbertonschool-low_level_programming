6cd309937492c7f9ca361bacc3a0bf9f090266db
David <davidlandg@gmail.com>
2021-11-02 09:23:36 -0500

    FIX - identation

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,11 +6,11 @@
  */
 void free_dog(dog_t *d)
 {
-    if (d == NULL)
-        return;
-    if (d->owner != NULL)
-        free(d->owner);
-    if (d->name != NULL)
-        free(d->name);
-    free(d);
+	if (d == NULL)
+		return;
+	if (d->owner != NULL)
+		free(d->owner);
+	if (d->name != NULL)
+		free(d->name);
+	free(d);
 }
