57fa0a6b7f938e653540cad72aa66cf3bd8e2322
David <davidlandg@gmail.com>
2021-11-03 22:04:08 -0500

    ADD - 3-calc.h 3-main.c 3-op_functions.c 3-get_op_func.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+/**
+ * main - print result of operation
+ * @argc: size of array
+ * @argv: data in array
+ * Return: success 0
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0, a = atoi(argv[1]), b = atoi(argv[3]);
+	char *op = argv[2];
+	int lenght = strlen(op);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[1] == NULL || argv[3] == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op != '+' && *op != '-' && *op != '*' && *op != '/' &&
+		 *op != '%') ||
+		(lenght > 1))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = get_op_func(argv[2])(a, b);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * op_add - sum a + b
+ * @a: first variable
+ * @b: second variable
+ * Return: result sum a + b
+ */
+int op_add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
+/**
+ * op_sub - sub a - b
+ * @a: first variable
+ * @b: second variable
+ * Return: result sub a - b
+ */
+int op_sub(int a, int b)
+{
+	int result = a - b;
+
+	return (result);
+}
+/**
+ * op_mul - mul a * b
+ * @a: first variable
+ * @b: second variable
+ * Return: result mul a * b
+ */
+int op_mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
+/**
+ * op_div - div a / b
+ * @a: first variable
+ * @b: second variable
+ * Return: result div a / b
+ */
+int op_div(int a, int b)
+{
+	int result;
+
+	if (a == 0 || b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = a / b;
+
+	return (result);
+}
+/**
+ * op_mod - mod a % b
+ * @a: first variable
+ * @b: second variable
+ * Return: result mod a % b
+ */
+int op_mod(int a, int b)
+{
+	int result;
+
+	if (a == 0 || b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = a % b;
+
+	return (result);
+}
