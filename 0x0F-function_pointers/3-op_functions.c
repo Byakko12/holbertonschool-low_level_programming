4482931b9abaa3d1f8ebe9e6b0c729ebbe1e9e60
David <davidlandg@gmail.com>
2021-11-03 22:28:33 -0500

    FIX - conditional

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -46,7 +46,7 @@ int op_div(int a, int b)
 {
 	int result;
 
-	if (a == 0 || b == 0)
+	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
@@ -66,7 +66,7 @@ int op_mod(int a, int b)
 {
 	int result;
 
-	if (a == 0 || b == 0)
+	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
