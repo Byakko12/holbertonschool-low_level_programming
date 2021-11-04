ab87403465c13ffe969c5b8ed9344fa1a7240b36
David <davidlandg@gmail.com>
2021-11-04 13:26:03 -0500

    1print_numbers.c README.md updates

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers, followed by a new line.
+ * @separator: separator of numbers
+ * @n: size of numbers
+ * Return: not return
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+
+	if (separator == NULL)
+	{
+		printf(" ");
+	}
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,9 @@
 #ifndef _HEADER_VARIADIC_
 #define _HEADER_VARIADIC_
 
+#include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
