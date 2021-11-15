0b1b4077d68c9a70da1056ad3783fb17dff3f471
David <davidlandg@gmail.com>
2021-11-14 22:35:12 -0500

    [FIX] - betty coding style

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,17 @@
 #include "lists.h"
-
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to data
+ * Return: returns the number of elements in a linked list_t list
+ */
 size_t list_len(const list_t *h)
 {
-    unsigned int i_elements = 0;
+	unsigned int i_elements = 0;
 
-    while (h)
-    {
-        i_elements++;
-        h = h->next;
-    }
-    return(i_elements);
+	while (h)
+	{
+		i_elements++;
+		h = h->next;
+	}
+	return (i_elements);
 }
