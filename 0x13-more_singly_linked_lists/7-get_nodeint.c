32b29d90f17a228541712bf0a99236c93133270b
David <davidlandg@gmail.com>
2021-11-16 11:32:29 -0500

    [FIX] - conditional in while

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,14 +9,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int counter = 0;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-
 	while (counter < index)
 	{
 		head = head->next;
+		if (head == NULL)
+			return (NULL);
 		counter++;
 	}
 	return (head);
