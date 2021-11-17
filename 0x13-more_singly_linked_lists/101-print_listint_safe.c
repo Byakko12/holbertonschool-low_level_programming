37aa60d0099fc63324878949aed2311337412be9
David <davidlandg@gmail.com>
2021-11-16 23:04:58 -0500

    [FIX] - betty coding style

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,27 +1,27 @@
 #include "lists.h"
 /**
- * *reverse_list - prints a listint_t linked list.
+ * print_listint_safe - prints a listint_t linked list.
  * @head: variable that point to type list
  * Return: a pointer to the first node of the reversed list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    unsigned int elements;
+	unsigned int elements;
 
-    if (head == NULL)
-    {
-        exit(98);
-    }
-    while (head != NULL)
-    {
-        printf("[%p] %d\n", (void*)head, head->n);
-        elements++;
-        if (head->next >= head)
-        {
-            printf("-> [%p] %d\n", (void*)head->next, head->next->n);
-            return (elements);
-        }
-        head = head->next;
-    }
-    return (elements);
+	if (head == NULL)
+	{
+		exit(98);
+	}
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		elements++;
+		if (head->next >= head)
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			return (elements);
+		}
+		head = head->next;
+	}
+	return (elements);
 }
