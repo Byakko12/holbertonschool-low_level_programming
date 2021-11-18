614358b9050065fa1ea26370f6aed6086cdd2981
David <davidlandg@gmail.com>
2021-11-18 18:25:37 -0500

    [ADD] - function binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ *
+ * @b: pointer to b
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int converted_to_decimal = 0;
+	int i = 0;
+	int factorial = 1;
+
+	while (b[i] != '\0')
+	{
+		if (b == NULL || (b[i] != '1' && b[i] != '0'))
+		{
+			return (0);
+		}
+		i++;
+	}
+	i--;
+	for (; b[i] != '\0'; i--)
+	{
+		converted_to_decimal += ((b[i] - '0') * factorial);
+		factorial = 2 * factorial;
+	}
+	return (converted_to_decimal);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef _HEADER_MAIN_
+#define _HEADER_MAIN_
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+#endif
