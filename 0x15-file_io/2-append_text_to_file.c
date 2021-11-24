7ef56051d93bff4089baaf92d5a8d04b33deea2b
David <davidlandg@gmail.com>
2021-11-23 20:34:07 -0500

    [FIX] - comparation return -1

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 	fd_open = open(filename, O_WRONLY | O_APPEND);
 	if (fd_open == -1)
